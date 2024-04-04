0025027347dfc5f314084d5bfd4b670d53b28357
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-04 18:43:55 +0300

    works

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -16,6 +16,22 @@ export default function App() {
     });
     setNewItem("")
   }
+
+  function togleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id === id) {
+          return { ...todo, completed }
+        }
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -32,13 +48,14 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
+      {todos.length === 0 && "No Todos"}
       {todos.map(todo => {
         return  <li key={todo.id}>
           <label>
-            <input type="checkbox" checked={todo.completed}/>
+            <input type="checkbox" checked={todo.completed} onChange={e => togleTodo(todo.id, e.target.checked)}/>
             {todo.title}
           </label>
-          <button className="btn btn-danger">Delete</button>
+          <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
         </li>
       })}
       </ul>
