94a7b7c0248a2136e4a48cc8144451390433a4d2
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-17 11:52:39 +0300

    Add new components: MyComponent and ObjectComponent

--- React-YouTube-Course/React-Guide/src/App.jsx
+++ React-YouTube-Course/React-Guide/src/App.jsx
@@ -1,6 +1,8 @@
 import Button from "./assets/Button/Button.jsx";
 import Student from "./props.jsx";
 import List from "./List.jsx";
+import MyComponent from "./useState.jsx";
+import ObjectComponent from "./objectUpdate.jsx";
 function App() {
   return (
     <>
@@ -9,6 +11,8 @@ function App() {
       <Student name="Gosho" age={77} isStudent={false} />
       <Student />
       <List />
+      <MyComponent />
+      <ObjectComponent />
     </>
   );
 }
